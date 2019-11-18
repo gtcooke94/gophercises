249787810aee7a3ed4f2c18524e3bfb23a4bd43b
Greg Cooke <gcooke@pindrop.com>
2019-11-18 12:00:26 -0500

    Full game running

--- blackjack/game/game.go
+++ blackjack/game/game.go
@@ -1,3 +1,4 @@
+//go:generate stringer -type=Action
 package blackjack
 
 import (
@@ -7,18 +8,23 @@ import (
 )
 
 type Player struct {
-	Cards      []deck.Card
-	Name       string
-	Chips      int
-	DealerFlag bool
+	Cards            []deck.Card
+	Name             string
+	Chips            int
+	DealerFlag       bool
+	DealerHiddenFlag bool
 }
 
 type Action uint8
+
+const MAXSCORE = 21
 
 const (
 	Hit Action = iota
 	Pass
 )
+
+var Actions = [...]Action{Hit, Pass}
 
 type Game struct {
 	Players  []Player
@@ -63,9 +69,16 @@ func StartGame(nPlayers int, nDecks int) *Game {
 }
 
 func (p Player) Score() int {
+	score, _ := scoreLogic(p.Cards)
+	return score
+}
+
+// Returns score, flag indicating if an ace counted as 11 or not
+func scoreLogic(cards []deck.Card) (int, bool) {
 	numAces := 0
 	score := 0
-	for _, card := range p.Cards {
+	aceAs11Flag := false
+	for _, card := range cards {
 		if card.Rank == deck.Ace {
 			numAces++
 		} else if card.Rank >= deck.Ten {
@@ -78,19 +91,70 @@ func (p Player) Score() int {
 	if score >= 11 {
 		score = score + numAces
 	} else if numAces >= 1 {
-		if score+11+numAces-1 > 21 {
+		if score+11+numAces-1 > MAXSCORE {
 			score = score + numAces
 		} else {
 			score = score + 11 + numAces - 1
+			aceAs11Flag = true
 		}
 	}
-	return score
+	return score, aceAs11Flag
+}
+
+func (p Player) String() string {
+	var ret string
+	fmt.Println("DEBUG", p.DealerHiddenFlag, p.DealerFlag)
+	if p.DealerHiddenFlag && p.DealerFlag {
+		ret = fmt.Sprintf("%s: Cards: %s, HIDDEN\n", p.Name, p.Cards[0])
+	} else {
+		ret = fmt.Sprintf("%s: Score: %d. Cards: %v\n", p.Name, p.Score(), p.Cards)
+	}
+	return ret
+}
+
+func (g *Game) PlayerTurn(p *Player, action Action) bool {
+	if (*p).Score() > MAXSCORE {
+		return false
+	}
+	switch action {
+	case Hit:
+		newCard := g.drawCard()
+		(*p).Cards = append((*p).Cards, newCard)
+		return true
+	case Pass:
+		return false
+	}
+	return false
+}
+
+func (g *Game) DealerTurn(p *Player) {
+	// Dealer hits on <=16, soft 17, holds otherwise
+	for hit := dealerHitCondition(*p); hit; {
+		newCard := g.drawCard()
+		(*p).Cards = append((*p).Cards, newCard)
+		hit = dealerHitCondition(*p)
+	}
+	(*p).DealerHiddenFlag = false
+	fmt.Println("DEBUG", (*p).DealerHiddenFlag)
+}
+
+func dealerHitCondition(p Player) bool {
+	if p.Score() <= 16 {
+		return true
+	} else if p.Score() == 17 {
+		_, soft17 := scoreLogic(p.Cards)
+		if soft17 {
+			return true
+		}
+	}
+	return false
+
 }
 
 func newPlayer(c []deck.Card, name string) Player {
-	return Player{Cards: c, Name: name, Chips: 0, DealerFlag: false}
+	return Player{Cards: c, Name: name, Chips: 0, DealerFlag: false, DealerHiddenFlag: false}
 }
 
 func newDealer(c []deck.Card) Player {
-	return Player{Cards: c, Name: "Dealer", Chips: int(math.Inf(1)), DealerFlag: true}
+	return Player{Cards: c, Name: "Dealer", Chips: int(math.Inf(1)), DealerFlag: true, DealerHiddenFlag: true}
 }
--- blackjack/main.go
+++ blackjack/main.go
@@ -1,26 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/gtcooke94/gophercises/blackjack/game"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Println("vim-go")
 	gamePtr := blackjack.StartGame(2, 2)
 	printPlayers((*gamePtr).Players)
+	takeTurns(gamePtr)
+	finalScoring(gamePtr)
 }
 
-func printPlayer(p blackjack.Player) {
-	if !p.DealerFlag {
-		fmt.Printf("%s: Score: %d. Cards: %v\n", p.Name, p.Score(), p.Cards)
-	} else {
-		fmt.Printf("%s: Cards: %s, HIDDEN\n", p.Name, p.Cards[0])
+func finalScoring(g *blackjack.Game) {
+	printPlayers((*g).Players)
+}
+
+func takeTurns(g *blackjack.Game) {
+	for i := range (*g).Players {
+		playerPtr := &((*g).Players[i])
+		continueTurn := true
+		if playerPtr.DealerFlag {
+			fmt.Println("====================")
+			(*g).DealerTurn(playerPtr)
+			continueTurn = false
+		}
+		for continueTurn {
+			fmt.Println("====================")
+			fmt.Println(*playerPtr)
+			printOptions()
+			action, err := acceptOption()
+			for err != nil {
+				fmt.Println("Invalid option selected. Please selection a valid option")
+				printOptions()
+				action, err = acceptOption()
+
+			}
+			continueTurn = (*g).PlayerTurn(playerPtr, blackjack.Action(action))
+		}
+	}
+}
+
+func acceptOption() (int, error) {
+	reader := bufio.NewReader(os.Stdin)
+	actionStr, _ := reader.ReadString('\n')
+	actionStr = strings.Replace(actionStr, "\n", "", -1)
+	action, err := strconv.Atoi(actionStr)
+	return action, err
+}
+
+func printOptions() {
+	for i, action := range blackjack.Actions {
+		fmt.Printf("%d. %s\n", i, action)
 	}
 }
 
 func printPlayers(players []blackjack.Player) {
 	for _, player := range players {
-		printPlayer(player)
+		fmt.Println(player)
 	}
 }
