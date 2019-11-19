e5e4a0bc5ffe70245bdaa9ea266fd1b981dca144
Greg Cooke <gcooke@pindrop.com>
2019-11-19 10:28:34 -0500

    Check in dealer.go

---
+++ blackjack/game/dealer.go
@@ -0,0 +1,33 @@
+package blackjack
+
+import (
+	"github.com/gtcooke94/gophercises/deck"
+	"math"
+)
+
+func (g *Game) DealerTurn(p *Player) {
+	// Dealer hits on <=16, soft 17, holds otherwise
+	for hit := dealerHitCondition(*p); hit; {
+		newCard := g.drawCard()
+		(*p).Cards = append((*p).Cards, newCard)
+		hit = dealerHitCondition(*p)
+	}
+	(*p).DealerHiddenFlag = false
+}
+
+func dealerHitCondition(p Player) bool {
+	if p.Score() <= 16 {
+		return true
+	} else if p.Score() == 17 {
+		_, soft17 := scoreLogic(p.Cards)
+		if soft17 {
+			return true
+		}
+	}
+	return false
+
+}
+
+func newDealer() Player {
+	return Player{Cards: make([]deck.Card, 0), Name: "Dealer", Chips: int(math.Inf(1)), DealerFlag: true, DealerHiddenFlag: true}
+}
